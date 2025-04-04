2de267337bdf30a7075b1eeb5d7b99841ae4e770
Andrey Urusov <114907800+AndreyUrus@users.noreply.github.com>
2025-04-04 11:33:54 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="backtester_defi",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    author="Andrey Urusov",
+    author_email="tapwi93@gmail.com",
+    description="Collection of useful functions for Uniswap v3 backtester",
+    url="https://github.com/AndreyUrus/backtester-defi",
+)
